8c9bdf3ea9942fd1edd5342b86701b011cfbd41e
Diego Maustem <diego.maustem@gmail.com>
2022-04-11 09:55:49 -0300

    configurações-iniciais

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+
+let firebaseConfig = {
+    apiKey: "AIzaSyDoMYFzxqJQMM-z31xBB3OgreE8unG20pE",
+    authDomain: "systemcalls-67e93.firebaseapp.com",
+    projectId: "systemcalls-67e93",
+    storageBucket: "systemcalls-67e93.appspot.com",
+    messagingSenderId: "1085403887004",
+    appId: "1:1085403887004:web:0c2bbf8edb39ecd5fa0459",
+    measurementId: "G-62CT4E9XVT"
+};
+
+if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+}  
+
+export default firebase;
