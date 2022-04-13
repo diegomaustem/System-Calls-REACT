ad0d28d5fee09c23a3a64812c5e95108a6d6e768
Diego Maustem <diego.maustem@gmail.com>
2022-04-13 10:54:05 -0300

    cirando-page-clientes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,3 @@
-
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 
@@ -7,6 +6,7 @@ import SignUp from '../pages/SignUp';
 
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
+import Customers from '../pages/Customers';
 
 export default function Routes(){
   return(
@@ -16,6 +16,7 @@ export default function Routes(){
 
       <Route exact path="/dashboard" component={Dashboard} isPrivate />
       <Route exact path="/profile" component={Profile} isPrivate />
+      <Route exact path="/customers" component={Customers} isPrivate />
     </Switch>
   )
 }
